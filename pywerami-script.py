040907556dfcc5aae4305aab83296764e4c5649f
ondrolexa <lexa.ondrej@gmail.com>
2017-01-18 00:34:23 +0100

    build fix

---
+++ pywerami-script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+from pywerami import mainapp
+
+def main():
+    print('Running pywerami...')
+    sys.exit(mainapp.main())
+
+if __name__=='__main__':
+    main()
+
