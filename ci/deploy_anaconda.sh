c1ae3f7770b63e0716e4c66228e8e27531138713
Ondrej Lexa <lexa.ondrej@gmail.com>
2017-12-05 10:57:38 +0100

    anaconda upload fix

--- ci/deploy_anaconda.sh
+++ ci/deploy_anaconda.sh
@@ -5,7 +5,7 @@
 set -e
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $CONDA_UPLOAD_TOKEN upload ~/conda-bld/**/apsg-*.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload --force $HOME/miniconda/conda-bld/**/pywerami-*.tar.bz2
 echo "Successfully deployed to Anaconda.org."
 
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
